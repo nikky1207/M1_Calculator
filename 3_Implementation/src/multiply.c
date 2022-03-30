ff873451741c44410afd129cbcda962052638919
nikky1207 <74638840+nikky1207@users.noreply.github.com>
2022-03-30 23:42:13 +0530

    Create multiply.c

---
+++ 3_Implementation/src/multiply.c
@@ -0,0 +1,11 @@
+int multiply()  
+{  
+    int n1, n2, res;  
+    printf (" The first number is: ");  
+    scanf ("  %d", &n1);  
+    printf (" The second number is: ");  
+    scanf ("  %d", &n2);  
+    res = n1 * n2;    
+    printf (" The multiply of %d * %d is: %d", n1, n2, res);  
+}  
+  
