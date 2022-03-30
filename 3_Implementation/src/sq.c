1413ebb70a58298062f9b9677321514b3b831194
nikky1207 <74638840+nikky1207@users.noreply.github.com>
2022-03-30 23:46:25 +0530

    Create sq.c

---
+++ 3_Implementation/src/sq.c
@@ -0,0 +1,11 @@
+int sq()  
+{  
+    int n1, res;  
+    printf (" Enter a number to get the Square: ");  
+    scanf ("  %d", &n1);  
+      
+    res = n1 * n1;    
+    printf (" \n The Square of %d is: %d", n1, res);  
+}  
+  
+// use sqrt1() function to get the square root of the given nu
