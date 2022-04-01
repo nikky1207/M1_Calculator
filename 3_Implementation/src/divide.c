665fecba76c38d15ac5dd8bcfec262fde9f32ea7
nikky1207 <74638840+nikky1207@users.noreply.github.com>
2022-04-01 18:00:13 +0530

    Update divide.c

--- 3_Implementation/src/divide.c
+++ 3_Implementation/src/divide.c
@@ -1,7 +1,7 @@
 #include "function.h"
 int divide()  
 {  
-    int n1, n2, res;  
+    float n1, n2, res;  
     printf (" The first number is: ");  
     scanf ("  %d", &n1);  
     printf (" The second number is: ");  
@@ -13,5 +13,5 @@ int divide()
         scanf ("%d", &n2);        
     }  
     res = n1 / n2;    
-    printf (" \n The division of %d / %d is: %d", n1, n2, res);  
+    printf (" \n The division of %d / %d is: %f", n1, n2, res);  
 }  
