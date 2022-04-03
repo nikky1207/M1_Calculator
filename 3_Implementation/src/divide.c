d612b2d52d9a3cb678b82064a91eed869296b989
nikky1207 <74638840+nikky1207@users.noreply.github.com>
2022-04-03 11:48:30 +0530

    Update divide.c

--- 3_Implementation/src/divide.c
+++ 3_Implementation/src/divide.c
@@ -3,15 +3,15 @@ int divide()
 {  
     float n1, n2, res;  
     printf (" The first number is: ");  
-    scanf ("  %d", &n1);  
+    scanf ("  %f", &n1);  
     printf (" The second number is: ");  
-    scanf ("  %d", &n2);  
+    scanf ("  %f", &n2);  
       
     if (n2 == 0)  
     {  
         printf (" \n Divisor cannot be zero. Please enter another value ");  
-        scanf ("%d", &n2);        
+        scanf ("%f", &n2);        
     }  
     res = n1 / n2;    
-    printf (" \n The division of %d / %d is: %f", n1, n2, res);  
+    printf (" \n The division of %f / %f is: %f", n1, n2, res);  
 }  
